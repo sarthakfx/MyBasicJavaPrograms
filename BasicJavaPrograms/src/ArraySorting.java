279687b3143caada1d8b0101b1c21b117524d9ef
SARTHAK <sarthakfx@gmail.com>
2017-08-14 23:14:37 +0530

    array

---
+++ BasicJavaPrograms/src/ArraySorting.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class ArraySorting {
+
+	public static void main(String[] args) {
+
+		int a[] = { 1, 4, 3, 2, 6, 4, 2, 8, 9, 6 };
+		Arrays.sort(a);
+		System.out.println(Arrays.toString(a));
+	}
+
+}
