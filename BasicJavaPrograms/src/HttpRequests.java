fcb3c10eca6dc5c62a230e6cb318bf2e4f6bc590
SARTHAK <sarthakfx@gmail.com>
2017-08-14 23:29:45 +0530

    Http Get REST

---
+++ BasicJavaPrograms/src/HttpRequests.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HttpRequests {
+
+	public static void main(String[] args) throws IOException {
+		String USER_AGENT = "Mozilla/5.0";
+		String GET_URL = "https://jsonplaceholder.typicode.com/posts/1";
+		URL obj = new URL(GET_URL);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		con.setRequestMethod("GET");
+		con.setRequestProperty("User-Agent", USER_AGENT);
+		int responseCode = con.getResponseCode();
+		System.out.println("GET Response Code :: " + responseCode);
+		if (responseCode == HttpURLConnection.HTTP_OK) { // success
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+
+			// print result
+			System.out.println(response.toString());
+		} else {
+			System.out.println("GET request not worked");
+		}
+
+	}
+
+}
