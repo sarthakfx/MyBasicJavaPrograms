fcc55391a0251bb3cdd2764b5863f44761bebdf9
SARTHAK <sarthakfx@gmail.com>
2017-08-15 19:10:16 +0530

    printff example

---
+++ BasicJavaPrograms/src/printf.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class printf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner sc=new Scanner(System.in);
+         System.out.println("================================");
+         for(int i=0;i<3;i++){
+             String s1=sc.next();
+             int x=sc.nextInt();
+             System.out.printf("%-15s%03d%n",s1 , x);
+         }
+         System.out.println("================================");
+	}
+
+}
+/*Sample Input
+
+java 100
+cpp 65
+python 50
+Sample Output
+
+================================
+java           100 
+cpp            065 
+python         050 
+================================*/
